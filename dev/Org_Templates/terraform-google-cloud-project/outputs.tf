3b03704351819dbcfa3384351041991dcfba607a
kdempire <128413009+Karandhawariya@users.noreply.github.com>
2024-11-07 23:52:20 +0530

    Delete dev/Org_Templates/terraform-google-cloud-project directory

--- dev/Org_Templates/terraform-google-cloud-project/outputs.tf
+++
@@ -1,7 +0,0 @@
-#...................................... Projects Creation Output .................................#
-
-output "Project_Details" {
-  description = "Details of the projects"
-  value       = module.project
-}
-
