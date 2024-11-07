8781d404c9fecfcdc18548db9229bf0a822f25d5
kdempire <128413009+Karandhawariya@users.noreply.github.com>
2024-11-07 19:32:48 +0530

    Update main.tf

--- dev/Org_Templates/terraform-google-cloud-apis/main.tf
+++ dev/Org_Templates/terraform-google-cloud-apis/main.tf
@@ -2,7 +2,7 @@ module "apis" {
       for_each = {
     for k,v  in try(var.apis,{}): k => v if v.delete != true  
   }
-    source = "../../Org_Module/terraform-google-cloud-apis"
+    source = "../../Org_Module/terraform-google-cloud-apis"  
     project_id              = each.key
     gcp_apis_list           = each.value.gcp_apis_list                           #["dialogflow.googleapis.com"]
     disable_apis_on_destroy = each.value.disable_apis_on_destroy                    #true
